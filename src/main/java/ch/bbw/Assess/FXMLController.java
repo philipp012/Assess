7466ac95908ac125ba138c0978453c19fa8c0c57
philipp012 <philipp.kiss@bluewin.ch>
2019-05-30 21:08:26 +0200

    added Gridpane to scrollpane and add button to gridpane when subject is added

--- src/main/java/ch/bbw/Assess/FXMLController.java
+++ src/main/java/ch/bbw/Assess/FXMLController.java
@@ -3,19 +3,30 @@ package ch.bbw.Assess;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 
 public class FXMLController implements Initializable {
 
     public ScrollPane scrollPane;
+    public GridPane gridPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        gridPane.setPadding(new Insets(25));
+        gridPane.setHgap(25);
+        gridPane.setVgap(25);
+
+    }
+
+    // Add subject
+    public void addSubject() {
+        Button button = new Button();
+        gridPane.add(button, 0, 0);
     }
 }
