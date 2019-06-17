3cec963d5643823b553d52ec67e3159eb98af7fd
5ia17masancassani <5ia17masancassani@gibwintra.ch>
2019-06-17 15:07:23 +0200

    add-notes changes

--- src/main/java/ch/bbw/Assess/Controller/NoteController.java
+++ src/main/java/ch/bbw/Assess/Controller/NoteController.java
@@ -1,29 +1,54 @@
 package ch.bbw.Assess.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.bbw.Assess.Models.Note;
 import ch.bbw.Assess.Models.Subject;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
 
 public class NoteController {
 
-    public TextField tfSubjectName;
-    private DashboardController parentController;
+	@FXML
+	public HTMLEditor htmlEditor;
 
-    void initialize(DashboardController parentController) {
-        this.parentController = parentController;
-    }
+	@FXML
+	private TextField subjectText;
 
-    public void confirmAddSubject(ActionEvent event) {
-        if (!tfSubjectName.getText().trim().isEmpty()) {
-            Subject subject = new Subject();
-            subject.setName(tfSubjectName.getText().trim());
-            parentController.addSubject(subject);
-            ((Node) (event.getSource())).getScene().getWindow().hide();
-        }
-    }
+	@FXML
+	private TextField titleText;
 
-    public void discard(ActionEvent event) {
-        ((Node) (event.getSource())).getScene().getWindow().hide();
-    }
+	private DashboardController parentController;
+	
+	List<Note> notes = new ArrayList<>();
+
+	void initialize(DashboardController parentController) {
+		this.parentController = parentController;
+	}
+
+	public void saveNote(ActionEvent event) {
+		if (!htmlEditor.getHtmlText().trim().isEmpty() && !subjectText.getText().trim().isEmpty()
+				&& !titleText.getText().trim().isEmpty()) {
+			
+			Note note = new Note(notes.size() + 1, subjectText.getText(), titleText.getText(), htmlEditor.getHtmlText());
+
+			//TODO: Subject Namen dürfen nicht mehrmals vorkommen
+			for (Subject subject : parentController.getSubjects()) {
+				if (subject.getName().toLowerCase().equals(this.subjectText.getText().toLowerCase())) {
+					subject.addNote(note);
+				}
+			}
+			notes.add(note);
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+		}
+	}
+
+	public void cancel(ActionEvent event) {
+		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
 }
--- src/main/java/ch/bbw/Assess/Models/Subject.java
+++ src/main/java/ch/bbw/Assess/Models/Subject.java
@@ -1,7 +1,10 @@
 package ch.bbw.Assess.Models;
 
+import java.util.List;
+
 public class Subject {
     private String name;
+    private List<Note> notes;
 
     public String getName() {
         return name;
@@ -10,4 +13,16 @@ public class Subject {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public List<Note> getNotes() {
+		return notes;
+	}
+
+	public void addNote(Note note) {
+		this.notes.add(note);
+	}
+
+	public void removeNote(Note note) {
+		this.notes.remove(note);
+	}
 }
