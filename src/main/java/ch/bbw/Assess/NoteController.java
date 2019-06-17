3e7998aa2b52677327266861e16c5240bb859e08
5ia17masancassani <5ia17masancassani@gibwintra.ch>
2019-06-17 13:32:02 +0200

    Note Scene

---
+++ src/main/java/ch/bbw/Assess/NoteController.java
@@ -0,0 +1,28 @@
+package ch.bbw.Assess;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+
+public class NoteController {
+
+    public TextField tfSubjectName;
+    private DashboardController parentController;
+
+    void initialize(DashboardController parentController) {
+        this.parentController = parentController;
+    }
+
+    public void confirmAddSubject(ActionEvent event) {
+        if (!tfSubjectName.getText().trim().isEmpty()) {
+            Subject subject = new Subject();
+            subject.setName(tfSubjectName.getText().trim());
+            parentController.addSubject(subject);
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        }
+    }
+
+    public void discard(ActionEvent event) {
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
+}
